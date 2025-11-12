d0249a7759194c76fd015ed49f9dfe8159410a6d
FE-csc <felipe.arayarivera@hotmail.com>
2025-11-11 18:20:52 -0600

    .Nueva Conexion

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli('localhost', 'root', '', 'Clinica_VisionDB');
+$mysqli = new mysqli('b8y9eccnoennbc03jxfn-mysql.services.clever-cloud.com', 'uonk3hj3tnpjdpfq', 'jywVsjXDFGN6cHLfQscM', 'b8y9eccnoennbc03jxfn');
 if ($mysqli->connect_error) {
     http_response_code(500);
     echo json_encode(['success' => false, 'message' => 'Error de conexión a la BD: ' . $mysqli->connect_error]);
