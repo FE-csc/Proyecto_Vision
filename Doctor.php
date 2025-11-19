9649c3e2559f822a3a5622247049ce5c35ae75a1
Johan Arroyo <johanarroyocruz14@gmail.com>
2025-11-18 18:23:41 -0600

    bugfix/reservacion sin logIn

--- Doctor.php
+++ Doctor.php
@@ -1,7 +1,12 @@
 <?php
+/**
+ * Inicia la sesion y valida si existe un usuario
+ * Si no existe, lo redirige al login con parametro redirect.
+ */
 session_start();
+
 if (!isset($_SESSION['user_id'])) {
-    header('Location: login.html');
+    header('Location: login.html?redirect=' . urldecode(basename($_SERVER['PHP_SELF'])));
     exit;
 }
 echo "hola doctor";
